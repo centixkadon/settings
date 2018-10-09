66ad69097efe80ff01de2f9ca4b621dea11c24ce
centixkadon <centixkadon@gmail.com>
2018-10-09 13:46:20 +0000

    Update Linux output log

--- Linux/script/ssserver.sh
+++ Linux/script/ssserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssserver -c /etc/shadowsocks/config.json --forbidden-ip 127.0.0.1/32,::1/128 --manager-address 127.0.0.1:11110 $*
+ssserver -c /etc/shadowsocks/config.json --forbidden-ip 127.0.0.1/32,::1/128 --manager-address 127.0.0.1:11110 $* 2>&1 |tee -a ~/All/log/shadowsocks.log
--- Linux/script/startup/shadowsocks.sh
+++ Linux/script/startup/shadowsocks.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-sleep 1
-screen -dmS ssserver ~/All/script/ssserver.sh
+case $1 in
+"start")
+  screen -dmS ssserver ~/All/script/ssserver.sh
 
-sleep 1
-screen -dmS sscount ~/All/script/sscount.sh
+  sleep 1
+  screen -dmS sscount ~/All/script/sscount.sh
 
-echo "$(date) | shadowsocks start" >> systemctl.log
+  echo "$(date) | shadowsocks start" >> ~/All/log/systemctl.log
+
+  ;;
+"stop")
+  screen -S ssserver -X quit
+  screen -S sscount -X quit
+
+  echo "$(date) | shadowsocks stop" >> ~/All/log/systemctl.log
+
+  ;;
+esac
