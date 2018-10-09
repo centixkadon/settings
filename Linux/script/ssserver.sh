88472db4cf3ccbb0f2967b6f58b8d2b997d81af1
centixkadon <centixkadon@gmail.com>
2018-10-10 00:35:54 +0800

    Update Linux remove some ssserver log

--- Linux/script/ssserver.sh
+++ Linux/script/ssserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssserver -c /etc/shadowsocks/config.json --forbidden-ip 127.0.0.1/32,::1/128 --manager-address 127.0.0.1:11110 $* 2>&1 |tee -a ~/All/log/shadowsocks.log
+ssserver -c /etc/shadowsocks/config.json --forbidden-ip 127.0.0.1/32,::1/128 --manager-address 127.0.0.1:11110 $* 2>&1 |grep -v "server already exists" |tee -a ~/All/log/shadowsocks.log
